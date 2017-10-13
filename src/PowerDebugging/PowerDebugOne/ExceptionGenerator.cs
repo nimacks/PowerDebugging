a898da882827da1ebfd86de18be4b99a260a1172
Samuel Mensah <samuelnmensah@live.com>
2017-10-13 16:09:13 -0700

    updated app

---
+++ src/PowerDebugging/PowerDebugOne/ExceptionGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerDebugOne
+{
+    /// <summary>
+    /// Generates a set of pre-defined Exceptions
+    /// </summary>
+    public static class ExceptionGenerator
+    {
+
+        /// <summary>
+        /// Generates a File Not Found Exception by attempting to open a non-existent File
+        /// </summary>
+        public static void GenerateFileNotFoundException()
+        {
+            try
+            {
+                var openFile = File.OpenRead(@"C:\non-existentFile.txt");
+            }
+            catch (FileNotFoundException)
+            {
+                throw;
+            }
+            
+        }
+
+        /// <summary>
+        /// Throws Divide by Zero Exception
+        /// </summary>
+        public static void GenerateDivideByZeroException()
+        {
+            try
+            {
+                int y = 0;
+                int b = 1;
+                int z = b / y;
+            }
+            catch (DivideByZeroException)
+            {
+
+                throw;
+            }
+        }
+    }
+}
--- src/PowerDebugging/PowerDebugOne/Program.cs
+++ src/PowerDebugging/PowerDebugOne/Program.cs
@@ -10,6 +10,15 @@ namespace PowerDebugOne
     {
         static void Main(string[] args)
         {
+            string str = "abcdef";
+            string start = getHalf(str).Insert(0, "ghij");
+            ExceptionGenerator.GenerateFileNotFoundException();
+            Console.ReadKey();
+        }
+
+        private static string getHalf(string str)
+        {
+            return str.Substring(str.Length / 2);
         }
     }
 }
